2dd2dcce6560424bd2241b05bdb9277488221f7b
Yin Da <yd219913@alibaba-inc.com>
2023-04-07 17:22:00 +0800

    Feat: migrate cluster
    Signed-off-by: Yin Da <yd219913@alibaba-inc.com>

---
+++ apis/oam/v1alpha1/cluster_types.go
@@ -0,0 +1,48 @@
+/*
+Copyright 2023 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	clusterv1alpha1 "github.com/oam-dev/cluster-gateway/pkg/apis/cluster/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Cluster is an extension model for cluster underlying secrets/ManagedClusters
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type Cluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec ClusterSpec `json:"spec,omitempty"`
+}
+
+// ClusterSpec spec of cluster
+type ClusterSpec struct {
+	Alias          string                         `json:"alias,omitempty"`
+	Accepted       bool                           `json:"accepted,omitempty"`
+	Endpoint       string                         `json:"endpoint,omitempty"`
+	CredentialType clusterv1alpha1.CredentialType `json:"credential-type,omitempty"`
+}
+
+// ClusterList list for Cluster
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type ClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Cluster `json:"items"`
+}
